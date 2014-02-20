79e08e6536bd69de864b46981ea8a91e6fad3939
Chris Lewis <bonsitm@gmail.com>
2014-02-20 17:45:19 +0000

    Added example project and screenshot

---
+++ example/src/alert.h
@@ -0,0 +1,31 @@
+/*********************************************************************/
+/* Simple alert mechanism for Pebble apps                            */
+/* Author: Chris Lewis                                               */
+/* Modified: 20/02/14                                                */
+/* Freely available to use with link to https://github.com/C-D-Lewis */
+/*********************************************************************/
+
+#include <pebble.h>
+
+#ifndef ALERT_H
+#define ALERT_H
+
+//Library constants
+#define MAX_ALERT_STRLEN_LENGTH 64	//For safety - largest string allowed in buffers
+
+//Library buffers
+char alert_lib_title_buffer[32];
+char alert_lib_body_buffer[64];
+
+//Library objects
+AppTimer *alert_lib_timer;
+TextLayer *alert_lib_title_layer, *alert_lib_body_layer;
+Layer *alert_lib_background_layer;
+
+//Function prototypes - see alert.c for details
+int alert_strlen(const char *string);
+void alert_show(Window *window, const char *title, const char *body, const int duration);
+void alert_update(const char *new_title, const char *new_body, const int new_duration);
+void alert_cancel();
+
+#endif
